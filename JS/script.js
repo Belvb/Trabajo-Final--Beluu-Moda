f88db9bd01fd7dd9b993af03a9074747e2face73
Belvb <89560037+Belvb@users.noreply.github.com>
2021-11-12 18:52:59 -0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+function descuento1() {alert("Felicidades! has obtenido 10% de descuento para tu próxima compra")
+
+}
+function descuento2() {alert("Felicidades! has obtenido 5% de descuento para tu próxima compra")
+
+}
+function descuento3() {alert("Felicidades! has obtenido 15% de descuento para tu próxima compra")
+
+}
+function descuento4() {alert("Mala Suerte! esta tarjeta no contiene descuentos intentalo otra vez mañana")
+
+}
